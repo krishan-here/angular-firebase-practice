598ee33d07f14391e898b08aadabc2cbfe3e5492
krishan-barnwal <iamkrishan111@gmail.com>
2023-02-27 19:46:26 +0530

    update panels

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,51 +5,37 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   customerDetail = CUSTOMER_DETAILS;
-  
   listOfPhones = CUSTOMER_DETAILS.listOfPhoneNumbers;
   phoneColumns = CUSTOMER_DETAILS.phonesColumn;
-
   listOfAddress = CUSTOMER_DETAILS.listOfAddress;
   addressColumns = CUSTOMER_DETAILS.addressColumn;
 
+  accountDetail = ACCOUNT_DETAILS;
+  listOfPaymentSchedules = ACCOUNT_DETAILS.paymentSchedules;
+  paymentColumns = ACCOUNT_DETAILS.paymentColumns;
 
-  accountDetailsColumn = ['account', 'type', 'legal', 'previous', 'application'];
-  currentTab = 'BASIC';
-  constructor(
-    private authService: AuthService,
-    private router: Router
-    ){}
+  currentTab = {
+    customer: 'BASIC',
+    account: 'BASIC',
+  };
 
-  ngOnInit(): void {
-    // this.getUserData(0, this.pageSize);
-  }
+  accountDetailsColumn = [
+    'account',
+    'type',
+    'legal',
+    'previous',
+    'application',
+  ];
+  constructor(private authService: AuthService, private router: Router) {}
 
-  goToTab(tab: string){
-    this.currentTab = tab;
-  }
+  ngOnInit(): void {}
 
-  // getUserData(pageIndex: number, pageSize: number){
-
-  // }
-
-  getVehicleDetails() {
-    console.log('vehicle details');
-    
-  }
-
-  getTransactionDetails() {
-    console.log('transaction data');
-  }
-  
-
-  logout(){
-    this.authService.logoutCustomer();
-    this.router.navigate(['/login']);
+  goToTab(panel: 'customer' | 'account', tab: string) {
+    this.currentTab[panel] = tab;
   }
 }
 
@@ -64,17 +50,17 @@ const CUSTOMER_DETAILS = {
     {
       type: 'PARMANENT',
       address: 'Durgapur, West Bengal-713363',
-      active: true
+      active: true,
     },
     {
       type: 'RESIDENT',
       address: 'H-6, Hyderabad',
-      active: false
+      active: false,
     },
     {
       type: 'PARMANENT',
       address: 'Indore, Madhya Pradesh-450002',
-      active: true
+      active: true,
     },
   ],
   listOfPhoneNumbers: [
@@ -85,7 +71,7 @@ const CUSTOMER_DETAILS = {
       timezone: 'ITC',
       relationship: 'N/A',
       inService: 'YES',
-      acNumber: '12893839293393'
+      acNumber: '12893839293393',
     },
     {
       type: 'phone',
@@ -94,7 +80,7 @@ const CUSTOMER_DETAILS = {
       timezone: 'SA',
       relationship: 'N/A',
       inService: 'YES',
-      acNumber: '39393839293393'
+      acNumber: '39393839293393',
     },
     {
       type: 'mobile',
@@ -103,13 +89,72 @@ const CUSTOMER_DETAILS = {
       timezone: 'ITC',
       relationship: 'N/A',
       inService: 'YES',
-      acNumber: '6723839293393'
+      acNumber: '6723839293393',
     },
   ],
 
+  phonesColumn: [
+    'type',
+    'phoneNumber',
+    'ext',
+    'timezone',
+    'relationship',
+    'inService',
+    'acNumber',
+  ],
+  addressColumn: ['type', 'address', 'active'],
+};
 
+const ACCOUNT_DETAILS = {
+  accountNumber: '19283992819283',
+  accountStatus: 'Active',
+  contractDate: '12-02-20',
+  closedDate: '12-02-24',
+  payoffDate: '10-03-20',
+  loanDetails: {
+    loanBalance: '12,000',
+    interestRecieableRounded: 'N/A',
+    accrualRate: '5%',
+    originalAmount: '45,0000',
+    overpaymentAmount: '23,000',
+    interestPaidLifetoDate: 'N/A',
+    payoffAmount: '2000',
+    APR: 'N/A',
+    maturityDate: '23-03-24',
+  },
+  paymentSchedules: [
+    {
+      startDate: '12-03-20',
+      endDate: '12-03-24',
+      frequency: 'high',
+      payments: 'Done',
+      amount: '2000',
+      paymentType: 'Normal',
+      leadDays: '35',
+    },
+    {
+      startDate: '12-03-20',
+      endDate: '12-03-24',
+      frequency: 'low',
+      payments: 'Pending',
+      amount: '2000',
+      paymentType: 'Normal',
+      leadDays: '29',
+    },
+    {
+      startDate: '12-03-20',
+      endDate: '12-03-24',
+      frequency: 'low',
+      payments: 'Done',
+      amount: '2000',
+      paymentType: 'Medium',
+      leadDays: '40',
+    }
+  ],
+  recentHistory: {
+    lastPaymentEffectiveDate: '12-04-22',
+    lastPaymentAmount: '23,000'
+  },
 
-  phonesColumn: ['type', 'phoneNumber', 'ext', 'timezone', 'relationship', 'inService', 'acNumber'],
-  addressColumn: ['type', 'address', 'active']
-}
-
+  paymentColumns: ['startDate', 'endDate', 'frequency', 'payments', 'amount', 'paymentType', 'leadDays']
+};
