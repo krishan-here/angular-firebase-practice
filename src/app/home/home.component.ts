3b4ec4d6779f5831604a7d5ba1ceeb588ecabf67
krishan-barnwal <iamkrishan111@gmail.com>
2023-04-28 10:20:04 +0530

    done some cdk

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,25 @@
-import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  
-  @ViewChild('formModal') formModal!: TemplateRef<any>;
+export class HomeComponent implements AfterViewInit{
+  @ViewChild('mat-tab-label-0-0', {static: false}) div!: ElementRef;
+
+  isCollapsed: boolean = true;
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private _focusMonitor: FocusMonitor
   ) {}
 
 
-
-
-  openDialog() {
-    const dialogRef = this.dialog.open(this.formModal);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
+    ngAfterViewInit() {
+        console.log(this.div.nativeElement);
+        
+        this._focusMonitor.stopMonitoring(this.div.nativeElement);
+    }
 }
